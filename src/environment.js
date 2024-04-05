b1e6b3dc0280b25ec9c035efbc618cde37c9750e
kumarsachin01 <kumarsachin.studio45@gmail.com>
2024-04-05 13:35:33 +0530

    First commit

---
+++ src/environment.js
@@ -0,0 +1,11 @@
+
+export const BACKEND_URL = "http://localhost:5000/";
+
+export const Frontend_URL = "http://localhost:3001/";
+
+
+// export const WEBSOCKET_URL = "ws://192.168.1.245:8039/ws/"   
+
+
+
+
