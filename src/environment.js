a724ad1a5b1ce2e316e06d6f8167d8533596268b
kumarsachin01 <kumarsachin.studio45@gmail.com>
2024-04-09 11:41:04 +0530

    tttt

--- src/environment.js
+++ src/environment.js
@@ -1,7 +1,7 @@
 
-export const BACKEND_URL = "http://localhost:5000/";
+export const BACKEND_URL = "https://e-med-care.vercel.app/";
 
-export const Frontend_URL = "http://localhost:3001/";
+export const Frontend_URL = "https://new-project-ten-tau.vercel.app/";
 
 
 // export const WEBSOCKET_URL = "ws://192.168.1.245:8039/ws/"   
